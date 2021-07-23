dbf9c9056d796d565106446c25d04a3f3cc96161
Renato-Nakamura <renato.yukio30@hotmail.com>
2021-07-23 15:37:19 -0300

    aula 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, ButtonChildren, ButtonEdit } from "./components/Button";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button/>
+      <Button/>
+      <ButtonEdit texto ={'olha, propriedades'}/>
+      <ButtonChildren>Children</ButtonChildren>
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,34 @@
+
+import { useState } from "react"
+
+//exemplo de estado em react, nele podemos alterar o proprio botao.
+export function Button(){
+    const [counter,setCounter] = useState(0)
+        
+    function increment(){
+        setCounter(counter+1);
+    }
+    return(
+        <button onClick={increment}>{counter}</button>
+    )
+}
+//Definindo as propriedades do botao para evitar erros 
+type ButtonProps = {
+    texto: string;
+}
+// Propriedades dos componentes que podem ser enviados
+export function ButtonEdit(props: ButtonProps) {
+    return(
+        <button>{props.texto}</button>
+    )
+}
+
+type ButtonChildrenprops = {
+    children: string;
+}
+//Propriedade de Children 
+export function ButtonChildren(props: ButtonChildrenprops) {
+    return(
+        <button>{props.children}</button>
+    )
+}
